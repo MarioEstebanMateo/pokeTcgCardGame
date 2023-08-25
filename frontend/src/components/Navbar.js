cf35d59d4c81a383b4098807bc8ed35d576ed885
Mario Esteban Mateo <memateo@gmail.com>
2023-08-25 12:17:47 -0300

    added hamburguer menu to navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,45 +5,65 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="container-fluid">
-      <ul className="liContainer">
-        <li>
-          <NavLink className="navLink" exact to="/" activeClassName="active">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className="navLink"
-            exact
-            to="/batalla5cartas"
-            activeClassName="active"
-          >
-            Batalla!
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className="navLink"
-            exact
-            to="/abrirsobre"
-            activeClassName="active"
-          >
-            Abrir Sobre
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className="navLink"
-            exact
-            to="/versetcompleto"
-            activeClassName="active"
-          >
-            Ver Set Completo
-          </NavLink>
-        </li>
-      </ul>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="liContainer navbar-nav">
+            <li>
+              <NavLink
+                className="navLink"
+                exact
+                to="/"
+                activeClassName="active"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="navLink"
+                exact
+                to="/batalla5cartas"
+                activeClassName="active"
+              >
+                Batalla!
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="navLink"
+                exact
+                to="/abrirsobre"
+                activeClassName="active"
+              >
+                Abrir Sobre
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="navLink"
+                exact
+                to="/versetcompleto"
+                activeClassName="active"
+              >
+                Ver Set Completo
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
